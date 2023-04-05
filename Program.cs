47896c87696bbf38a438daabd8f015af57d22068
yusuf gürcan 1180505810 <92579500+Joseph0grcn@users.noreply.github.com>
2023-04-05 23:24:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Bellman_Ford_Algoritms
     {
         public struct Edge
         {
-            public int Source, Destination, Weight;
+            public int Kaynak, Hedef, Weight;
         };
 
         public static void BellmanFord(Edge[] edges, int numberOfEdges, int numberOfVertices, int sourceVertex)
@@ -29,8 +29,8 @@ namespace Bellman_Ford_Algoritms
             {
                 for (int j = 0; j < numberOfEdges; ++j)
                 {
-                    int u = edges[j].Source;
-                    int v = edges[j].Destination;
+                    int u = edges[j].Kaynak;
+                    int v = edges[j].Hedef;
                     int weight = edges[j].Weight;
 
                     if (distance[u] != INFINITY && distance[u] + weight < distance[v])
@@ -42,8 +42,8 @@ namespace Bellman_Ford_Algoritms
 
             for (int i = 0; i < numberOfEdges; ++i)
             {
-                int u = edges[i].Source;
-                int v = edges[i].Destination;
+                int u = edges[i].Kaynak;
+                int v = edges[i].Hedef;
                 int weight = edges[i].Weight;
 
                 if (distance[u] != INFINITY && distance[u] + weight < distance[v])
@@ -56,31 +56,39 @@ namespace Bellman_Ford_Algoritms
             Console.WriteLine("Düğümlere olan en kısa mesafeler:");
             for (int i = 0; i < numberOfVertices; ++i)
             {
-                Console.WriteLine("{0} - {1}", i, distance[i]);
+
+                Console.WriteLine(i + " - " + distance[i]);
             }
         }
 
         public static void Main()
         {
             Edge[] edges = {
-            //Kenarlar ve ağırlıkları
-            new Edge { Source = 0, Destination = 1, Weight = -1 },
-            new Edge { Source = 0, Destination = 2, Weight = 4 },
-            new Edge { Source = 1, Destination = 2, Weight = 3 },
-            new Edge { Source = 1, Destination = 3, Weight = 2 },
-            new Edge { Source = 1, Destination = 4, Weight = 2 },
-            new Edge { Source = 3, Destination = 2, Weight = 5 },
-            new Edge { Source = 3, Destination = 1, Weight = 1 },
-            new Edge { Source = 4, Destination = 3, Weight = -3 }
+            //Kenarlar ve ağırlıkları belirliyoruz
+            //Buradaki Weight'e maliyet denilebilir. ama tam tanımı bir kaynaktan bir hedefe mesafe ve maliyet toplamıdır.
+            //örneğin A'dan B'ye kısa ama paralı bir yoldan gitmek uzun ama parasız bir yoldan gitmeye göre daha maliyetli olabilir.
+            new Edge { Kaynak = 0, Hedef = 1, Weight = 1 },
+            new Edge { Kaynak = 0, Hedef = 2, Weight = -4 },
+            new Edge { Kaynak = 1, Hedef = 2, Weight = 3 },
+            new Edge { Kaynak = 1, Hedef = 3, Weight = 2 },
+            new Edge { Kaynak = 1, Hedef = 4, Weight = 2 },
+            new Edge { Kaynak = 3, Hedef = 2, Weight = -5 },
+            new Edge { Kaynak = 3, Hedef = 1, Weight = 1 },
+            new Edge { Kaynak = 4, Hedef = 3, Weight = 3 }
         };
 
+            //gidilmek istenen nokta sayısı 
             int numberOfVertices = 5;
+
+            //gidilebilecek yol sayısı
             int numberOfEdges = 8;
 
+            //başlangıç kaynak noktası
             int sourceVertex = 0;
 
+            //fonksiyonumuzu çağırıyoruz
             BellmanFord(edges, numberOfEdges, numberOfVertices, sourceVertex);
-            Console.ReadLine    ();
+            Console.ReadLine();
         }
     }
 }
